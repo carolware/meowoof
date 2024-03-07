a4473856c73fcb712690a3cfcd88b90367dc8de1
carol <carolinepxavier03@gmail.com>
2024-03-07 10:35:17 -0300

    adicionando responsividade a header e criando novo componente home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import logoMeowoof from '../assets/logoMeowoof.png';
 import { Link } from 'react-scroll';
 
-//icones
+// Ícones
+import { FaXmark, FaBars } from 'react-icons/fa6';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -22,7 +23,7 @@ const Navbar = () => {
     };
     window.addEventListener('scroll', handleScroll);
     return () => {
-      window.addEventListener('scroll', handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
@@ -35,31 +36,45 @@ const Navbar = () => {
 
   return (
     <>
-      <header className="w-full bg-mainOrange fixed top-0 lef-0 right-0">
-        <nav>
-          <div>
-            <a href="">
+      <header className={`w-full bg-mainOrange fixed top-0 left-0 right-0 ${isSticky ? 'border-b' : ''}`}>
+        <nav className={`py-0 lg:px-14 px-4 ${isSticky ? 'sticky top-0 left-0 right-0 border-b duration-300' : ''}`}>
+          <div className="flex justify-between items-center">
+            <a href="/">
               <img
                 className="w-16 inline-block items-center object-contain"
                 src={logoMeowoof}
                 alt="logo do abrigo cachorro e gatinho"
               />
             </a>
-            <ul className="md:flex space-x-12 hidden">
+
+            <ul className={`md:flex md:space-x-12 md:justify-center items-center hidden ${isMenuOpen ? 'block' : 'hidden'}`}>
               {navItems.map(({ link, path }) => (
-                <Link
-                  to={path}
-                  spy={true}
-                  smooth={true}
-                  offset={-100}
-                  key={path}
-                  className="block text-base text-black hover:text-white"
-                >
-                  {link}
-                </Link>
+                <li key={path}>
+                  <Link
+                    to={path}
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    className="block text-sm text-black hover:text-white"
+                  >
+                    {link}
+                  </Link>
+                </li>
               ))}
             </ul>
-        
+
+            <div className="md:hidden">
+              <button
+                onClick={toggleMenu}
+                className="focus:outline-none focus:text-black"
+              >
+                {isMenuOpen ? (
+                  <FaXmark className="h-6 w-6 text-black" />
+                ) : (
+                  <FaBars className="h-6 w-6 text-black" />
+                )}
+              </button>
+            </div>
           </div>
         </nav>
       </header>
