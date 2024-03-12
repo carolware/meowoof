8802ae4dda45c43bad5568155c28ad4f6e7070bf
carol <carolinepxavier03@gmail.com>
2024-03-12 11:18:40 -0300

    adicionando botão e descrições

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,21 +3,22 @@ import React from 'react';
 import Navbar from './Navbar';
 import Elipse from '../assets/Elipse.png'; 
 import Dog from '../assets/Dog.png'; 
+import { FiArrowRight } from 'react-icons/fi'
 
 
 
 const Home = () => {
   return (
-    <>
+    <div className="home-container">
       <Navbar />
-      <div className="mt-28"> {/* Adicione uma margem superior à div */}
-        <img className='w-1/4 h-1/2 lg:w-1/4 md:h-1/2' src={Elipse} alt="elipse"/>
+      <div className='home-text-section mt-28'>
+        <h1 className='primary-subheading'>Não compre,</h1>
+        <h1 className='secondary-subheading '>adote!</h1>
+        <p className='primary-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed sunt consectetur accusantium porro? Dolores, odit commodi quasi corporis consequuntur saepe porro voluptate exercitationem, vero laudantium praesentium, deserunt molestiae quod minus.</p>
+        <button className='secondary-button'>Adote agora mesmo <FiArrowRight/></button>
       </div>
-      <div className="-mt-96"> {/* Adicione uma margem superior à div */}
-        <img className='w-1/4 h-1/2 lg:w-1/4 md:h-1/2' src={Dog} alt="elipse"/>
-      </div>
-    </>
+    </div>
+    
   );
 };
-
 export default Home;
